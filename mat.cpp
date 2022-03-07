a6fb26d1b66871d66dde86b9a522c68cbe2c66bc
einav242 <93386470+einav242@users.noreply.github.com>
2022-03-07 19:23:05 +0200

    Add files via upload

---
+++ mat.cpp
@@ -0,0 +1,13 @@
+#ifndef SystemB_EX1_MAT_HPP
+#define SystemB_EX1_MAT_HPP
+
+#include <string>
+using  namespace std;
+
+namespace ariel{
+    string mat(int c, int r, char Symbol1,char Symbol2){
+        return "";
+    };
+}
+
+#endif //MATALA_MAT_HP
---
+++ mat.hpp
@@ -0,0 +1,11 @@
+#ifndef System_EX1_MAT_HPP
+#define System_EX1_MAT_HPP
+
+#include <string>
+
+namespace ariel{
+    std::string mat(int c, int r, char Symbol1,char Symbol2);
+}
+
+
+#endif //MATALA_MAT_HP
